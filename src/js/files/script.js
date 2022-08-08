02bc5a598d5be246189fd4cfdee5e552bf1d4384
OksanaZhuravel <opuseva@mail.ru>
2022-08-08 21:24:48 +0400

    new page added

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,7 +1,7 @@
 // Подключение функционала "Чертогов Фрилансера"
 // import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-// import { flsModules } from './modules.js';
+import { flsModules } from './modules.js';
 
 document.querySelectorAll('.menu__link').forEach(function (activ) {
   activ.addEventListener('click', function (e) {
